efc43f29c6208856227af618e57ff0781843e62a
‪Ehab Yousef‬‏ <74505597+ehabyousef@users.noreply.github.com>
2024-06-11 17:43:35 +0300

    plate images

--- wannameal/src/pages/Auth/Auth.jsx
+++ wannameal/src/pages/Auth/Auth.jsx
@@ -5,19 +5,22 @@ import Account from './Account/Account';
 import Forgot from './Forgot/Forgot';
 import Verification from './Verification/Verification';
 import Reset from './Reset/Reset';
+import plate1 from "../../assets/plate_1.png"
+import plate2 from "../../assets/plate_2.png"
+import plate3 from "../../assets/plate_3.png"
 export default function Auth() {
 
     return (
         <div className={style.thebody}>
             <Link to='/' className={style.logo}>Wanna Meal</Link>
             <div className={style.plate_1}>
-                <img src="../../assets/plate_1.png" alt="plate" />
+                <img src={plate1} alt="plate" />
             </div>
             <div className={style.plate_2}>
-                <img src='../../assets/plate_2.png' alt="plate" />
+                <img src={plate2} alt="plate" />
             </div>
             <div className={style.plate_3}>
-                <img src='../../assets/plate_3.png' alt="plate" />
+                <img src={plate3} alt="plate" />
             </div>
             <Routes>
                 <Route path='/' element={< Account />} />
